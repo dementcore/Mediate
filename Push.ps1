10636b6f9d35fae5cf0c2b39f4a0fcc3495c9bbc
DementCore <53424423+dementcore@users.noreply.github.com>
2021-05-17 14:18:12 +0200

    Fix

--- Push.ps1
+++ Push.ps1
@@ -1,9 +1,6 @@
-$artifacts = ".\artifacts"
-if(Test-Path $artifacts) { Remove-Item $artifacts -Force -Recurse }
-
 if ([string]::IsNullOrEmpty($Env:NUGET_API_KEY)) {
     Write-Host "NUGET_API_KEY is empty."
 } else {
-    dotnet pack $Env:PROJECT_PATH -c Release -o $artifacts --no-build
-    dotnet nuget push $artifacts --source $Env:NUGET_URL --api-key $Env:NUGET_API_KEY
+    dotnet pack $Env:PROJECT_PATH -c Release -o Temp --no-build
+    dotnet nuget push Temp/* --source $Env:NUGET_URL --api-key $Env:NUGET_API_KEY
 }
