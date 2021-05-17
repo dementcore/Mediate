07c1e054ca30e2b2581db92e1ce8397fd80be106
DementCore <53424423+dementcore@users.noreply.github.com>
2021-05-17 14:21:53 +0200

    Fix push script

--- Push.ps1
+++ Push.ps1
@@ -2,5 +2,11 @@ if ([string]::IsNullOrEmpty($Env:NUGET_API_KEY)) {
     Write-Host "NUGET_API_KEY is empty."
 } else {
     dotnet pack $Env:PROJECT_PATH -c Release -o Temp --no-build
-    dotnet nuget push Temp/* --source $Env:NUGET_URL --api-key $Env:NUGET_API_KEY
+      Get-ChildItem Temp\ -Filter "*.nupkg" | ForEach-Object {
+        Write-Host "Pushing $($_.Name)"
+        dotnet nuget push $_ --source $Env:NUGET_URL --api-key $Env:NUGET_API_KEY
+        if ($lastexitcode -ne 0) {
+            throw ("Exec: " + $errorMessage)
+        }
+    }
 }
